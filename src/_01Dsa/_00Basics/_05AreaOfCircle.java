abad1e18f2588fcffc6e78e50a4516fea8f4420c
arka <arkagarai292@gmail.com>
2025-12-08 11:24:03 +0530

    yoo

--- src/_01Dsa/_00Basics/_05AreaOfCircle.java
+++ src/_01Dsa/_00Basics/_05AreaOfCircle.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class _05AreaOfCircle {
   public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter value of radius(r) : ");
-      double r = sc.nextDouble();
-      System.out.println("Area of circle : " + Math.PI*Math.pow(r,2)+" sq. units");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter value of radius(r) : ");
+    double r = sc.nextDouble();
+    System.out.println("Area of circle : " + Math.PI * Math.pow(r, 2) + " sq. units");
   }
 }
